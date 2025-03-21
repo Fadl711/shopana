4e17c7b7a2ab357e11610ced5eb4f5077860713e
Fadl711 <fathlvvee@gmail.com>
2025-03-21 23:39:18 +0300

    Df

--- config/filesystems.php
+++ config/filesystems.php
@@ -40,7 +40,7 @@ return [
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
             'region' => env('AWS_DEFAULT_REGION', 'auto'),
-            'bucket' => env('AWS_BUCKET'),
+            'bucket' => 'fls-9e7aa932-bd81-418f-bc2c-459da5e705c6', // TEMPORARY HARDCODED VALUE
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'url' => env('AWS_URL'),
